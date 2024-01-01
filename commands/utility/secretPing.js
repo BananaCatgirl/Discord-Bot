927e5ec30d72c60dddac55e744c44812ee8c2e1a
Jenny <jenny.koppers99@gmail.com>
2024-01-01 22:55:14 +0100

    stuff done

---
+++ commands/utility/secretPing.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('secret-ping')
+		.setDescription('secretly replies with Pong!'),
+	async execute(interaction)
+	{
+		await interaction.reply({ content: 'secret Pong!', ephemeral: true });
+	},
+};
